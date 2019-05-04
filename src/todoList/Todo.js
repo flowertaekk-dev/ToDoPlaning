ddb4903dd059c6902f7221714404ee5953365b33
flowertaekk.dev <flowertaekk.dev@gmail.com>
2019-05-04 20:38:46 +0900

    css updated

--- src/todoList/Todo.js
+++ src/todoList/Todo.js
@@ -32,16 +32,16 @@ class Todo extends Component {
                     <caption>ToDo</caption>
                     <tbody>
                         <tr>
-                            <th scope='row' className={this._table}>TODO</th>
-                            <td className={this._table}>{this.props.todo}</td>
+                            <th scope='row'>TODO</th>
+                            <td>{this.props.todo}</td>
                         </tr>
                         <tr>
-                            <th scope='row' className={this._table}>Dead-line</th>
-                            <td className={this._table}>{this.props.deadLine}</td>
+                            <th scope='row'>Dead-line</th>
+                            <td>{this.props.deadLine}</td>
                         </tr>
                         <tr>
-                            <th scope='row' className={this._table}>Details</th>
-                            <td className={this._table}>{this.props.taskDetails}</td>
+                            <th scope='row'>Details</th>
+                            <td>{this.props.taskDetails}</td>
                         </tr>
                     </tbody>
                 </table>
