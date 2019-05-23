8b45fbe19212f8a8baff9a811419847819436b3b
flowertaekk.dev <flowertaekk.dev@gmail.com>
2019-05-24 01:06:16 +0900

    refactoring Header.js completed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './Header.css';
 
-class Header extends Component {
+const Header = (props) => {
+    return (
+        <div className='Header'>
 
-    render () {
-        return (
-            <div className='Header'>
-                <div className='title'>
-                    <h1>It's ToDoPlanning!</h1>
-                </div>
-                <div className='menu'>
-                    {   
-                        // Code modifier - JuYoung Kang
-                        this.props.signOut ?
-                        <ul>
-                            <li>
-                                <label>{this.props.userId}</label>
-                            </li>
-                            <li className='cursor'>
-                                <button onClick={this.props.signOut}>sign out</button>
-                            </li>
-                        </ul>
-                        : null
-                    }
-                </div>
+            <div className='title'>
+                <h1>It's ToDoPlanning!</h1>
             </div>
-        )
-    }
+
+            <div className='menu'>
+                {   
+                    // Code modifier - JuYoung Kang
+                    props.signOut ?
+                    <ul>
+                        <li>
+                            <label>{props.userId}</label>
+                        </li>
+                        <li className='cursor'>
+                            <button onClick={props.signOut}>sign out</button>
+                        </li>
+                    </ul>
+                    : null
+                }
+            </div>
+            
+        </div>
+    )
  }
 
 export default Header
